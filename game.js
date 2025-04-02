f24caced30bee67a9c45c80b9ddce6c46f6d1292
sharmaketan03 <ketan301024@gmail.com>
2025-04-02 13:40:19 +0530

    complete project

--- game.js
+++ game.js
@@ -3,16 +3,16 @@ let gayab = document.querySelector(".gayab");
 
 let timescore = document.querySelector("#gayab");
 
-
-
 let avatar1 = document.querySelectorAll("img");
 let imagearea = document.querySelector(".imagesArea");
-let timer=document.querySelector("#time")
-let scored=document.querySelector("#score")
-console.log(timer)
+let timer = document.querySelector("#time");
+let scored = document.querySelector("#score");
+let score1=document.querySelector("#score1")
+
+console.log(timer);
 let selectedAvatar;
 let imageInterval;
-let sum=0
+let sum = 0;
 let score = 0;
 
 startgame.addEventListener("click", () => {
@@ -24,77 +24,67 @@ startgame.addEventListener("click", () => {
 
   screen1.classList.add("gayab");
   screen2.classList.remove("gayab");
-  
 
- 
-
-  
   for (let i = 0; i < avatar1.length; i++) {
     avatar1[i].addEventListener("click", () => {
       // console.log(avatar1[i]);
       screen3.classList.remove("gayab");
       screen2.classList.add("gayab");
-      selectedAvatar= avatar1[i];
+      selectedAvatar = avatar1[i];
 
       // console.log(selectedAvatar)
-    //  setInterval(timeadd,1000)  
-    imageInterval=setInterval(generateImage,1000);
+      //  setInterval(timeadd,1000)
+      imageInterval = setInterval(generateImage, 1000);
     });
-  
   }
-  
- 
-function generateImage(){
-  
-  let images =document.createElement("img");
-  images.classList.add("objectimg")
-  images.style.left=getARandomNumber("x")+"px"
-  // images.style.left = "20px"
-  images.style.top=getARandomNumber("y")+"px"
-  // images.style.left = "20px"
 
-  // console.log(getARandomNumber());
-  if(sum>=30){
+  function generateImage() {
+    let images = document.createElement("img");
+    images.classList.add("objectimg");
+    images.style.left = getARandomNumber("x") + "px";
+    // images.style.left = "20px"
+    images.style.top = getARandomNumber("y") + "px";
+    // images.style.left = "20px"
 
-    alert(`Time's up final score is ${score}`)
-       clearInterval(imageInterval)
-      
-      
+    // console.log(getARandomNumber());
+    if (sum >= 5) {
+      console.log(score)
+         score1.innerText=score
+      clearInterval(imageInterval);
+
+      imagearea.innerText = "";
+      screen2.classList.remove("gayab");
+      screen3.classList.add("gayab");
+    
+     
+    } else {
+      sum = sum + 1;
+      timer.innerText = sum;
+    }
+    scored.innerText = score;
+
+    images.src = selectedAvatar.src;
+    console.log(images);
+
+    images.addEventListener("click", () => {
+      score += 1;
+      scored.innerText = score;
+
+      images.remove();
+    });
+
+    imagearea.append(images);
   }
-  else{
-    sum=sum+1
-    timer.innerText=sum
-  }
-  scored.innerText=score;
-  
-  images.src=selectedAvatar.src;
-  console.log(images)
-  images.addEventListener("click",()=>{
-    score +=1;
-    scored.innerText=score
-
-    images.remove()  
-})
-  imagearea.append(images) 
-}
-
 });
 
 function getARandomNumber(axis) {
-
   const ran = Math.floor(Math.random() * 1440);
   // console.log(ran);
   if (axis === "x") {
-
-   
-      if (ran < 50 || ran > 1400) return getARandomNumber(axis)
-      else return ran
-     
-  }
-  else {
-      if (ran < 50 || ran > 380) return getARandomNumber(axis)
-      else return ran
+    if (ran < 50 || ran > 1400) return getARandomNumber(axis);
+    else return ran;
+  } else {
+    if (ran < 50 || ran > 380) return getARandomNumber(axis);
+    else return ran;
   }
 }
-
-
